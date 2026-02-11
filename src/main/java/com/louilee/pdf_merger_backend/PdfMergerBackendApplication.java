337b649755df465d88cdafe0536f2c4d1999ec8f
Lee, Keonhyeong <Keonhyeong.Lee@adesso.de>
2026-02-11 18:21:57 +0100

    init

---
+++ src/main/java/com/louilee/pdf_merger_backend/PdfMergerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.louilee.pdf_merger_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfMergerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfMergerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/louilee/pdf_merger_backend/PdfMergerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louilee.pdf_merger_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfMergerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
